3ad8fca049ea1f23864f260aeacd68f438996b62
Hans-J. Schmid <knock@myopendoor.de>
2025-03-27 17:19:13 +0100

    bacon

--- ch1/1/1_basic_print.cpp
+++ ch1/1/1_basic_print.cpp
@@ -5,14 +5,14 @@
 void
 PrintFunc(int x)
 {
-    std::cout << x << '\n';
+    std::cout << x << ' ';
 }
 
 int
 main()
 {
     std::vector<int> v;
-    v.push_back(1); // no uniform initialisation in C++03!
-    v.push_back(2); // push_back available only... :)
-    std::for_each(v.begin(), v.end(), PrintFunc);
+    v.push_back(1);                               // no uniform initialisation in C++03!
+    v.push_back(2);                               // push_back available only... :)
+    std::for_each(v.begin(), v.end(), PrintFunc); // 1 2
 }
